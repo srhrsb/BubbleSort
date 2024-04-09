078f30e517424129e1766f71fbd478a978295f27
srhrsb <sebastian.boettcher2@srh.de>
2024-04-09 16:33:54 +0200

    Bubble Sort

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
+import java.util.Timer;
 
 public class Main {
     public static void main(String[] args) {
           int[] a = {9,3,4,7,6,2,1};
           System.out.println( Arrays.toString(a) );
+
           int[] b = bubbleSort(a);
           System.out.println( Arrays.toString(b) );
     }
@@ -18,7 +20,7 @@ public class Main {
            while(sorted != true){
                sorted = true;
                for(int i = 0; i < array.length-1; i++){
-                   if(array[i] < array[i+1]){
+                   if(array[i] > array[i+1]){
                        int temp = array[i+1];
                        array[i+1] = array[i];
                        array[i] = temp;
