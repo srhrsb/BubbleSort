9233c252e63ee9ccd0205aae0443b7ed8790dafb
srhrsb <sebastian.boettcher2@srh.de>
2024-04-10 09:17:04 +0200

    Bubble Sort kommentiert

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
-import java.util.Timer;
 
 public class Main {
     public static void main(String[] args) {
+
           int[] a = {9,3,4,7,6,2,1};
           System.out.println( Arrays.toString(a) );
 
@@ -16,14 +16,27 @@ public class Main {
      * @return array, welches sortiert ist
      */
     private static int[] bubbleSort( int[] array ){
-           boolean sorted = false;
+        //Hilfsvariable um sich zu merken,
+        // ob weiteres sortieren notwendig ist
+        boolean sorted = false;
+
+        //die While-Schleife läuft solange,
+        //wie sorted false ist
            while(sorted != true){
-               sorted = true;
+               sorted = true; //das würde bedeuten,
+               // dass das sortieren endet
+
                for(int i = 0; i < array.length-1; i++){
+
+                   //es wird geprüft, ob der Vorgänger im Array
+                   //größer ist als der Nachfolger
                    if(array[i] > array[i+1]){
+                       //ja -> Werte tauschen
                        int temp = array[i+1];
                        array[i+1] = array[i];
                        array[i] = temp;
+
+                       //es soll weiter sortiert werden
                        sorted = false;
                    }
                }
